8f45fe1d5b0ec706ebb4a78f622475b604193432
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2024-11-12 11:53:17 -0800

    wow

--- components/core/Menu.tsx
+++ components/core/Menu.tsx
@@ -104,23 +104,25 @@ export default async function Menu() {
 	)
 }
 
-const ListItem = forwardRef<React.ElementRef<'a'>, React.ComponentPropsWithoutRef<'a'>>(({ className, title, children, ...props }, ref) => {
-	return (
-		<li>
-			<NavigationMenuLink asChild>
-				<a
-					ref={ref}
-					className={twMerge(
-						'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
-						className
-					)}
-					{...props}
-				>
-					<div className="text-sm font-medium leading-none">{title}</div>
-					{children && <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">{children}</p>}
-				</a>
-			</NavigationMenuLink>
-		</li>
-	)
-})
+const ListItem = forwardRef<React.ElementRef<typeof Link>, React.ComponentPropsWithoutRef<typeof Link>>(
+	({ className, title, children, ...props }, ref) => {
+		return (
+			<li>
+				<NavigationMenuLink asChild>
+					<Link
+						ref={ref}
+						className={twMerge(
+							'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
+							className
+						)}
+						{...props}
+					>
+						<div className="text-sm font-medium leading-none">{title}</div>
+						{children && <p className="text-sm leading-snug line-clamp-2 text-muted-foreground">{children}</p>}
+					</Link>
+				</NavigationMenuLink>
+			</li>
+		)
+	}
+)
 ListItem.displayName = 'ListItem'
